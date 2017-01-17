23cf8dc1fe92b7484735f2f907be39ea2a96faaa
oleno <pirit.lena@gmail.com>
2017-01-17 16:24:59 +0200

    creating inner Star class and defining the logic of behavior, looping move in the view screen

--- core/src/com/mygdx/game/Background.java
+++ core/src/com/mygdx/game/Background.java
@@ -2,8 +2,28 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class Background {
+    class Star {
+        private Vector2 position; // position.x and position.y
+        private float speed;
+
+        public Star() {
+            position = new Vector2((float)Math.random() * 1280,
+                                   (float)Math.random() * 720);
+            speed = 5.0f;
+        }
+
+        public void update() {
+            position.x -= speed;
+            if (position.x < 0) {
+                position.x = 1280;
+                position.y = (float)Math.random() * 720;
+            }
+        }
+    }
+
     private Texture texture;
 
     public Background() {
