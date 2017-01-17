e5de53f19f00d42bd34898eb85e0bdeef5a0a133
oleno <pirit.lena@gmail.com>
2017-01-17 15:54:32 +0200

    creating Background class with background image

--- core/src/com/mygdx/game/Background.java
+++ core/src/com/mygdx/game/Background.java
@@ -1,4 +1,16 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class Background {
+    private Texture texture;
+
+    public Background() {
+        texture = new Texture("backspace.png");
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, 0, 0);
+    }
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,17 +3,16 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Background background;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("backspace.png");
+		background = new Background();
 	}
 
 	@Override
@@ -22,7 +21,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1); // RGBA
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		background.render(batch);
 		batch.end();
 	}
 
