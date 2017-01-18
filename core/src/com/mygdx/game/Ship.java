33990faf0a31ecc1ac026cb7585cbb4dbb164192
oleno <pirit.lena@gmail.com>
2017-01-18 14:00:59 +0200

    ship rotates little bit by moving up and down

--- core/src/com/mygdx/game/Ship.java
+++ core/src/com/mygdx/game/Ship.java
@@ -10,30 +10,41 @@ public class Ship {
     private Vector2 position;
     private Texture texture;
     private float speed;
+    private float angle;
 
     public Ship() {
         position = new Vector2(100, 100);
         texture = new Texture("ship.png");
         speed = 10.0f;
+        angle = 0.0f;
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, position.x, position.y);
+        batch.draw(texture, position.x, position.y,
+                    75, 55, 155, 115, 1.0f, 1.0f,
+                    angle, 0, 0, 155, 115, false, false);
+//        batch.draw(texture, position.x, position.y);
     }
 
     public void update() {
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            angle += 1.0f;
+            if (angle > 15f) angle = 15f;
             position.y += speed;
             if (position.y > 720) {
                 position.y = -120;
             }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            angle -= 1.0f;
+            if (angle < 25f) angle = -25f;
             position.y -= speed;
             if (position.y < 0) {
                 position.y = 840;
             }
         }
+        if (angle > 0) angle -= 0.5f;
+        if (angle < 0) angle += 0.5f;
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             position.x -= speed;
             if (position.x < 0) {
