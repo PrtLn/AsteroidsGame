9130aaf532c67d7df2d64e99c76810edf72b8481
oleno <pirit.lena@gmail.com>
2017-01-18 16:30:36 +0200

    creating Bullet class

---
+++ core/src/com/mygdx/game/Bullet.java
@@ -0,0 +1,36 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Bullet {
+    private Vector2 position;
+    private float speed;
+    private boolean active;
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setup(int x, int y) {
+        position.x = x;
+        position.y = y;
+        active = true;
+    }
+
+    public Bullet() {
+        position = new Vector2(0f, 0f);
+        speed = 7.0f;
+        active = false;
+    }
+
+    public void destroy() {
+        active = false;
+    }
+
+    public void update() {
+        position.x += speed;
+        if (position.x > 1280) {
+            destroy();
+        }
+    }
+}
